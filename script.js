dc5016d80fa98dfbb999c64d1cc6137686dd34aa
Burkeonis <Burke.JF7@icloud.com>
2026-07-16 07:02:04 -0600

    Rebuild Burkeonis ecosystem and song archive

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const printButton = document.querySelector('#print-workbook');
 if (printButton) printButton.addEventListener('click', () => window.print());
+
+const audioPlayers = document.querySelectorAll('audio');
+audioPlayers.forEach((player) => {
+  player.addEventListener('play', () => {
+    audioPlayers.forEach((otherPlayer) => {
+      if (otherPlayer !== player && !otherPlayer.paused) otherPlayer.pause();
+    });
+  });
+});
